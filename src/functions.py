16c379b2509460e79f18527d24cbff7362587994
christodharma <13521009@std.stei.itb.ac.id>
2023-02-24 14:38:48 +0700

    divide and conquer 2 dimensi, belum selesai

--- src/functions.py
+++ src/functions.py
@@ -3,10 +3,12 @@ import random
 import math
 from mpl_toolkits import mplot3d
 
+#static int callEuclideanctr        how?
+
 #constructor set of titik
 #nTitik mewakilkan berapa titik yang diinginkan
 #nDimensi mewakilkan berapa dimensi titik dibuat (default: 3)
-def setOfTitik(nTitik, nDimensi=3):
+def randomSetOfTitik(nTitik, nDimensi=3):
     result = []
     for i in range(nTitik):
         point = [random.randint(0,10) for j in range(nDimensi)]
@@ -22,4 +24,39 @@ def euclideanDistance(aTitik, bTitik):
             result += (aTitik[i]-bTitik[i])**2
         return math.sqrt(result)
 
-#static int callEuclideanctr
+#sorting untuk proses divide
+#selalu terurut membesar
+#https://scripteverything.com/python-2d-list-sort-by-multiple-columns-code-examples-no-imports-one-liners/
+#tidak mengubah data input
+def sortBySumbu(setTitik, sumbu):
+    if (sumbu == 'x' or sumbu == 'X'):
+        sumbu = 1
+    elif (sumbu == 'y' or sumbu == 'Y'):
+        sumbu = 2
+    elif (sumbu == 'z' or sumbu == 'Z'):
+        sumbu = 3
+    newSetTitik = sorted(setTitik, key=lambda row:(row[sumbu-1]))
+    return newSetTitik
+
+#divide
+#membagi set titik menjadi 2 bagian pada bagian tengah
+def divide(setTitik):
+    half = len(setTitik)//2
+    return setTitik[:half], setTitik[half:]
+
+#divide and conquer
+#mencari pasangan titik yang paling dekat
+#belum memperhitungkan kalau yang terdekat tepat di garis divide
+def findClosestPair(setTitik):
+    if (len(setTitik) == 2):
+        return euclideanDistance(setTitik[0], setTitik[1]), setTitik    #perlu bisa return setTitiknya itu sendiri buat visualisasi
+    else:
+        d1,d2 = divide(setTitik)
+        return min(findClosestPair(d1), findClosestPair(d2))      #ini bisa terpengaruh kalo juga return setTitik
+                                                                        #bisa pengaruh penggunaan memori?
+
+
+def printTitik(titikAsArr):
+    print("(", end = "")
+    print(*titikAsArr, sep = ', ', end="")
+    print(")", end="")
--- src/main.py
+++ src/main.py
@@ -5,6 +5,21 @@ from mpl_toolkits import mplot3d
 import functions as fun
 
 banyakTitik = int(input("banyak titik : "))
-setOfTitik = fun.setOfTitik(banyakTitik)
-print(setOfTitik)
-print("distance between :", setOfTitik[0], "and", setOfTitik[-1], ":", fun.euclideanDistance(setOfTitik[0], setOfTitik[-1]))
+if (input("3 dimensi atau tidak? (Y/N) ") not in "Yy"):
+    setOfTitik = fun.randomSetOfTitik(banyakTitik, int(input("oke! masukkan dimensi: ")))
+else:
+    setOfTitik = fun.randomSetOfTitik(banyakTitik)  
+print("setOfTitik:\n",setOfTitik)
+
+#nyoba 2 dimensi
+newSetOfTitik = fun.sortBySumbu(setOfTitik, 'x')
+closestDistance, closestPair = fun.findClosestPair(newSetOfTitik)
+print("Divide and conquer untuk mencari pasangan titik terdekat dari\n", newSetOfTitik, "\ndidapat jarak terdekat:", closestDistance, "dengan titik terdekatnya adalah", end = " ")
+fun.printTitik(closestPair[0])
+print(" dan ", end="")
+fun.printTitik(closestPair[1])
+
+#buat dimensi 3 atau lebih caranya gimana?
+#bagi set titik berdasarkan sumbu x, y, z (slice horizontal, vertical, etc) secara gantian
+#jadi percabangannya bukan 2 kali tapi jadi 8 kali ...(percabangan = rekursi)
+#banyak percabangannya jadi 2^n-1 dimensi??
