327b606e0afb3133772a2a7fd1ec987eeba9d769
Angger Ilham <angger.ilhamamanullah004@gmail.com>
2023-02-25 18:10:34 +0700

    fix euclideancount, add bonus1.py

--- src/bonus1.py
+++ src/bonus1.py
@@ -1,9 +1,53 @@
 import math
 import random
 import time
+import matplotlib.pyplot as plt
 
 euclideancount = 0
 startTime = time.time()
+
+
+def showcartesian(titikterdekat,titik) :
+    fig = plt.figure()
+    x = []
+    y = []
+    z = []
+    xclose = []
+    yclose = []
+    zclose = [] 
+
+    # masukin yg pasangan terdekat
+    for i in range(len(titikterdekat)) :
+        for j in range(0,3) :
+            if(j == 0) :
+                xclose.append(titikterdekat[i][j])
+            elif(j == 1) :
+                yclose.append(titikterdekat[i][j])
+            else :
+                zclose.append(titikterdekat[i][j])
+
+    # masukin yg lain
+    for i in range(len(titik)) :
+        for j in range(0,3) :
+            if(j == 0 and titik[i][j] != xclose[0] and titik[i][j] != xclose[1]) :
+                x.append(titik[i][j])
+            elif(j == 1 and titik[i][j] != yclose[0] and titik[i][j] != yclose[1]) :
+                y.append(titik[i][j])
+            elif(j == 2 and titik[i][j] != zclose[0] and titik[i][j] != zclose[1]) :
+                z.append(titik[i][j])
+    ax = plt.axes(projection='3d')
+    ax.scatter(x,y,z, c = 'k')
+    ax.scatter(xclose,yclose,zclose, c = 'r')
+    
+
+    ax.set_title('Diagram kartesian')
+    ax.set_xlabel('Sumbu X')
+    ax.set_ylabel('Sumbu Y')
+    ax.set_zlabel('Sumbu Z')
+
+    plt.show()
+
+
 
 
 # caclculate distance
@@ -86,3 +130,11 @@ print("Pasangan titik terdekat : ", titikterdekat)
 print("Jaraknya : ", jaraktitikterdekat)
 print("banyak perhitungan euclidean : ", euclideancount)
 print(f"Lama program berjalan : {stopTime-startTime:.6f} detik")
+
+g = int(input("\nmau tampilin grafik ? 1.ya, 2.tidak : "))
+
+if(g == 1) :
+    showcartesian(titikterdekat,titik)
+    print("ok.")
+else :
+    print("ok.")
