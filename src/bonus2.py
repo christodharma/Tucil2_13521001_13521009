bdeca62927a35e4d5f80690afe07d7b8eeebb897
christodharma <13521009@std.stei.itb.ac.id>
2023-02-27 22:23:56 +0700

    added bruteforce, changed files, changed time

--- src/bonus2.py
+++ src/bonus2.py
@@ -3,26 +3,74 @@ import random
 import time
 import numpy as np
 
-euclideancount = 0
-startTime = time.time()
-
-
-# caclculate distance
+# calculate distance
 def hitungjarak(point1, point2) :
-    global euclideancount
-    euclideancount += 1
     jarak = 0
     for i in range(len(point1)) :
         jarak += (point1[i] - point2[i])**2
-
+    global euclideancount
+    euclideancount += 1
     return math.sqrt(jarak)
 
 
 # find closest point
-def closestpoint(titik, dimensi) :
-    
-    banyaktitik = len(titik)
-    if banyaktitik <=3 : # memakai bruteforce kalau ada 3 atau kurang titik
+def closestpoint(titik, dimensi, divideandconquer = True) :
+    if (divideandconquer):    
+        banyaktitik = len(titik)
+        if banyaktitik <=3 : # memakai bruteforce kalau ada 3 atau kurang titik
+            jarakminimum = float("inf")
+            for i in range(banyaktitik) :
+                for j in range(i+1, banyaktitik) :
+                    jar = hitungjarak(titik[i],titik[j]) 
+                    if(jar < jarakminimum) :
+                        jarakminimum = jar
+                        titikterdekat = (titik[i], titik[j])
+            return titikterdekat, jarakminimum
+        
+
+        else :
+            # membagi titik jadi 2 bagian
+            tengah = banyaktitik //2
+            bagiankiri = titik[:tengah]
+            bagiankanan = titik[tengah:]
+
+            # manggil fungsi closestpoint lagi untuk tiap bagian
+            bagiankiriterdekat, jarbagiankiriterdekat = closestpoint(bagiankiri, dimensi)
+            bagiankananterdekat, jarbagiankananterdekat = closestpoint(bagiankanan, dimensi)
+
+            # jarak terdekat antara bagian kiri dan kanan
+
+            jarak = min(jarbagiankiriterdekat, jarbagiankananterdekat)
+
+            # pasangan titik terdekat yang ada didekat garis pembagi
+            titikgaristengah = []
+            for i in range(dimensi) :
+                titikgaristengah.append(titik[tengah][i])
+
+            titiktengah = []
+            
+            for n in titik :
+                if abs(n[0] - titikgaristengah[0]) < jarak :
+                    titiktengah.append(n)
+            
+            jaraktitiktengahterdekat = float("inf")
+            for i in range(len(titiktengah)) :
+                for j in range(i+1, min(i + 8, len(titiktengah))) :
+                
+                    jarak = hitungjarak(titiktengah[i], titiktengah[j])
+                    if(jarak < jaraktitiktengahterdekat) :
+                        jaraktitiktengahterdekat = jarak
+                        titiktengahterdekat = (titiktengah[i], titiktengah[j])
+            
+            # return pasangan titik terdekat
+            
+            if jaraktitiktengahterdekat < jarak :
+                return titiktengahterdekat, jaraktitiktengahterdekat
+            elif hitungjarak(bagiankiriterdekat[0], bagiankiriterdekat[1]) < hitungjarak(bagiankananterdekat[0], bagiankananterdekat[1]) :
+                return bagiankiriterdekat, hitungjarak(bagiankiriterdekat[0], bagiankananterdekat[1])
+            else : return bagiankananterdekat, hitungjarak(bagiankananterdekat[0], bagiankananterdekat[1])  
+    elif (not divideandconquer): #strategi bruteforce
+        banyaktitik = len(titik)
         jarakminimum = float("inf")
         for i in range(banyaktitik) :
             for j in range(i+1, banyaktitik) :
@@ -30,51 +78,7 @@ def closestpoint(titik, dimensi) :
                 if(jar < jarakminimum) :
                     jarakminimum = jar
                     titikterdekat = (titik[i], titik[j])
-        return titikterdekat, jarakminimum
-    
-
-    else :
-        # membagi titik jadi 2 bagian
-        tengah = banyaktitik //2
-        bagiankiri = titik[:tengah]
-        bagiankanan = titik[tengah:]
-
-        # manggil fungsi closestpoint lagi untuk tiap bagian
-        bagiankiriterdekat, jarbagiankiriterdekat = closestpoint(bagiankiri, dimensi)
-        bagiankananterdekat, jarbagiankananterdekat = closestpoint(bagiankanan, dimensi)
-
-        # jarak terdekat antara bagian kiri dan kanan
-
-        jarak = min(jarbagiankiriterdekat, jarbagiankananterdekat)
-
-        # pasangan titik terdekat yang ada didekat garis pembagi
-        titikgaristengah = []
-        for i in range(dimensi) :
-            titikgaristengah.append(titik[tengah][i])
-
-        titiktengah = []
-        
-        for n in titik :
-            if abs(n[0] - titikgaristengah[0]) < jarak :
-                titiktengah.append(n)
-        
-        jaraktitiktengahterdekat = float("inf")
-        for i in range(len(titiktengah)) :
-            for j in range(i+1, min(i + 8, len(titiktengah))) :
-               
-               jarak = hitungjarak(titiktengah[i], titiktengah[j])
-               if(jarak < jaraktitiktengahterdekat) :
-                   jaraktitiktengahterdekat = jarak
-                   titiktengahterdekat = (titiktengah[i], titiktengah[j])
-        
-        # return pasangan titik terdekat
-        
-        if jaraktitiktengahterdekat < jarak :
-            return titiktengahterdekat, jaraktitiktengahterdekat
-        elif hitungjarak(bagiankiriterdekat[0], bagiankiriterdekat[1]) < hitungjarak(bagiankananterdekat[0], bagiankananterdekat[1]) :
-               return bagiankiriterdekat, hitungjarak(bagiankiriterdekat[0], bagiankananterdekat[1])
-        else : return bagiankananterdekat, hitungjarak(bagiankananterdekat[0], bagiankananterdekat[1])  
-
+        return titikterdekat, jarakminimum        
 
 
 # main program
@@ -86,11 +90,21 @@ for i in range(n) :
         titik[i][j] = random.uniform(0,1)
 
 # i untuk banyak titik, j dimensinya
-
+euclideancount = 0
+startTime = time.process_time()
 titikterdekat, jaraktitikterdekat = closestpoint(titik,m)
-
-stopTime = time.time()
-print("Pasangan titik terdekat : ", titikterdekat[0], titikterdekat[1])
+stopTime = time.process_time()
+print("Pasangan titik terdekat : ", titikterdekat)
 print("Jaraknya : ", jaraktitikterdekat)
 print("banyak perhitungan euclidean : ", euclideancount)
-print(f"Lama program berjalan : {stopTime-startTime:.6f} detik")
+print(f"Lama program berjalan : {(stopTime-startTime)*(1000):.6f} ms")
+
+euclideancount = 0
+startTime = time.process_time()
+titikterdekat_bruteforce, jaraktitikterdekat_bruteforce = closestpoint(titik,m, False)
+stopTime = time.process_time()
+print("---------------BRUTEFORCE---------------")
+print("Pasangan titik terdekat : ", titikterdekat_bruteforce)
+print("Jaraknya : ", jaraktitikterdekat_bruteforce)
+print("banyak perhitungan euclidean : ", euclideancount)
+print(f"Lama program berjalan : {(stopTime-startTime)*(1000):.6f} ms")
--- src/main.py
+++ src/main.py
@@ -1,25 +1,102 @@
-#main.py
-import random
 import math
-from mpl_toolkits import mplot3d
-import functions as fun
+import random
+import time
 
-banyakTitik = int(input("banyak titik : "))
-if (input("3 dimensi atau tidak? (Y/N) ") not in "Yy"):
-    setOfTitik = fun.randomSetOfTitik(banyakTitik, int(input("oke! masukkan dimensi: ")))
-else:
-    setOfTitik = fun.randomSetOfTitik(banyakTitik)  
-print("setOfTitik:\n",setOfTitik)
+# calculate distance
+def hitungjarak(point1, point2) :
+    global euclideancount
+    euclideancount += 1
+    x1, y1, z1 = point1
+    x2, y2, z2 = point2
+    return math.sqrt((x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2)
 
-#nyoba 2 dimensi
-newSetOfTitik = fun.sortBySumbu(setOfTitik, 'x')
-closestDistance, closestPair = fun.findClosestPair(newSetOfTitik)
-print("Divide and conquer untuk mencari pasangan titik terdekat dari\n", newSetOfTitik, "\ndidapat jarak terdekat:", closestDistance, "dengan titik terdekatnya adalah", end = " ")
-fun.printTitik(closestPair[0])
-print(" dan ", end="")
-fun.printTitik(closestPair[1])
 
-#buat dimensi 3 atau lebih caranya gimana?
-#bagi set titik berdasarkan sumbu x, y, z (slice horizontal, vertical, etc) secara gantian
-#jadi percabangannya bukan 2 kali tapi jadi 8 kali ...(percabangan = rekursi)
-#banyak percabangannya jadi 2^n-1 dimensi??
+# find closest point
+def closestpoint(titik, divideandconquer=True) :
+    if(divideandconquer):    
+        banyaktitik = len(titik)
+        if banyaktitik <=3 : # memakai bruteforce kalau ada 3 atau kurang titik
+            jarakminimum = float("inf")
+            for i in range(banyaktitik) :
+                for j in range(i+1, banyaktitik) :
+                    jar = hitungjarak(titik[i],titik[j]) 
+                    if(jar < jarakminimum) :
+                        jarakminimum = jar
+                        titikterdekat = (titik[i], titik[j])
+            return titikterdekat, jarakminimum
+        
+
+        else :
+            # membagi titik jadi 2 bagian
+            tengah = banyaktitik //2
+            bagiankiri = titik[:tengah]
+            bagiankanan = titik[tengah:]
+
+            # manggil fungsi closestpoint lagi untuk tiap bagian
+            bagiankiriterdekat, jarbagiankiriterdekat = closestpoint(bagiankiri)
+            bagiankananterdekat, jarbagiankananterdekat = closestpoint(bagiankanan)
+
+            # jarak terdekat antara bagian kiri dan kanan
+
+            jarak = min(jarbagiankiriterdekat, jarbagiankananterdekat)
+
+            # pasangan titik terdekat yang ada didekat garis pembagi
+            titikgaristengah = (titik[tengah][0], titik[tengah][1], titik[tengah][2])
+            titiktengah = []
+            for n in titik :
+                if abs(n[0] - titikgaristengah[0]) < jarak :
+                    titiktengah.append(n)
+            
+            jaraktitiktengahterdekat = float("inf")
+            for i in range(len(titiktengah)) :
+                for j in range(i+1, min(i + 8, len(titiktengah))) :
+                    jarak = hitungjarak(titiktengah[i], titiktengah[j])
+                    if(jarak < jaraktitiktengahterdekat) :
+                        jaraktitiktengahterdekat = jarak
+                        titiktengahterdekat = (titiktengah[i], titiktengah[j])
+            
+            # return pasangan titik terdekat
+            
+            if jaraktitiktengahterdekat < jarak :
+                return titiktengahterdekat, jaraktitiktengahterdekat
+            elif hitungjarak(bagiankiriterdekat[0], bagiankiriterdekat[1]) < hitungjarak(bagiankananterdekat[0], bagiankananterdekat[1]) :
+                return bagiankiriterdekat, hitungjarak(bagiankiriterdekat[0], bagiankananterdekat[1])
+            else : return bagiankananterdekat, hitungjarak(bagiankananterdekat[0], bagiankananterdekat[1])  
+    elif (not divideandconquer): #strategi bruteforce
+        banyaktitik = len(titik)
+        jarakminimum = float("inf")
+        for i in range(banyaktitik) :
+            for j in range(i+1, banyaktitik) :
+                jar = hitungjarak(titik[i],titik[j]) 
+                if(jar < jarakminimum) :
+                    jarakminimum = jar
+                    titikterdekat = (titik[i], titik[j])
+        return titikterdekat, jarakminimum
+
+# main program
+n = int(input("Masukkan jumlah titik : "))
+titik = []
+for i in range(n) :
+    x = random.uniform(0,1)
+    y = random.uniform(0,1)
+    z = random.uniform(0,1)
+    titik.append((x,y,z))
+
+euclideancount = 0
+startTime = time.process_time()
+titikterdekat, jaraktitikterdekat = closestpoint(titik)
+stopTime = time.process_time()
+print("Pasangan titik terdekat : ", titikterdekat)
+print("Jaraknya : ", jaraktitikterdekat)
+print("banyak perhitungan euclidean : ", euclideancount)
+print(f"Lama program berjalan : {(stopTime-startTime)*(1000):.6f} ms")
+
+euclideancount = 0
+startTime = time.process_time()
+titikterdekat_bruteforce, jaraktitikterdekat_bruteforce = closestpoint(titik, False)
+stopTime = time.process_time()
+print("---------------BRUTEFORCE---------------")
+print("Pasangan titik terdekat : ", titikterdekat_bruteforce)
+print("Jaraknya : ", jaraktitikterdekat_bruteforce)
+print("banyak perhitungan euclidean : ", euclideancount)
+print(f"Lama program berjalan : {(stopTime-startTime)*(1000):.6f} ms")
